88252304cbea8e861ffded157325a73598f01bdc
Eswari1421104 <eswaribaskar24@gmail.com>
2017-06-05 18:02:40 +0530

    Update and rename OddEven.java to PNZ.java

--- PNZ.java
+++ PNZ.java
@@ -4,15 +4,20 @@ public class Array {
       public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        if(n>=0)
+        if(n==0)
                     {
-            System.out.println("positive");
+            System.out.println("zero");
                   
         }
-        else
+        else if(n<0)
+        {
             System.out.println("negative");
         
+        }
+        else
+            System.out.println("positive");
+      }
         
-    }
+    
     
 }
